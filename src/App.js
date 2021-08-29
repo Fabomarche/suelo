f637b4f95f3751019d3b5593ddde28e5e53c698f
Fabomarche <fabriciomarche@hotmail.com>
2021-08-29 17:52:31 -0300

    correciones intro sumÃe carousel

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import NavBar from './components/NavBar'
 import Intro from './components/Intro'
+import Carousel_intro from './components/Carousel_intro';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header>
         <NavBar />
-        <Intro />
       </header>
+      <main className="container justify-content-center">
+        <Intro />
+        <Carousel_intro />
+      </main>    
     </div>
   );
 }
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import introSuelo from '../assets/images/intro_suelo.png'
-import {Row, Form} from 'react-bootstrap'
+import {Row, Form, Container} from 'react-bootstrap'
 
 const Intro = () => {
     return (
-        <Row className="justify-content-center mt-3">
+        <Row className="container justify-content-center mt-3">
+            <Container className="d-flex justify-content-center">
             <img src={introSuelo} alt="logo suelo mercado organico" />
+            </Container>
+            
             <Form className="w-50 mt-3" >
                 <Form.Group >
                     <Form.Control className="text-center" type="search" placeholder="¿Qué querés comer hoy?"/>
