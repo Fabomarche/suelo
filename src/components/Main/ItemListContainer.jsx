03ea3988a78cf3518bd8195bae9ea23af5801102
Fabomarche <fabriciomarche@hotmail.com>
2021-09-14 18:00:05 -0300

    correcion desafio clase 6 item e item list con Promise useEffect etc

--- src/components/Main/ItemListContainer.jsx
+++ src/components/Main/ItemListContainer.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { productsFetch } from '../utils/mock'
 import ItemList from '../Products/ItemList'
 
+import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Placeholder from 'react-bootstrap/Placeholder'
+import Spinner from 'react-bootstrap/Spinner'
+
+
 const ItemListContainer = ({ mensaje }) => {
+    const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        productsFetch
+        .then(respuesta =>{
+            setProductos(respuesta)
+        })
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false))
+    }, [])
+
     return (
         <>
-            <ItemList />
+            <Container className="d-flex justify-content-center">
+            { loading ? <Card style={{ width: '18rem' }} className="text-center shadow border-5 m-2" border="primary">
+                            <Container class="container flex d-flex">
+                                <Spinner animation="border" variant="primary"className="m-3" />
+                                <Card.Title>Cargando...</Card.Title>
+                            </Container>
+                            <Card.Body>
+                                <Placeholder as={Card.Text} animation="glow">
+                                    <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
+                                    <Placeholder xs={6} /> <Placeholder xs={8} />
+                                </Placeholder>
+                                <Placeholder.Button variant="primary" xs={6} />
+                            </Card.Body>
+                        </Card> 
+                    : <ItemList productos={productos}/> }
+            </Container>
+        
             <h2 className="text-center mt-5">{ mensaje }</h2>
         </>
     )
--- src/components/Products/Item.jsx
+++ src/components/Products/Item.jsx
@@ -1,46 +1,24 @@
 import React, { useState } from 'react'
+
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
-import Placeholder from 'react-bootstrap/Placeholder'
-import Spinner from 'react-bootstrap/Spinner'
 
-
-const Item = ({ products, loading }) => {
-    
-    
-
+const Item = ({ producto }) => {
     return (
-        <Container className="d-flex justify-content-center">
-        { loading ? <Card style={{ width: '18rem' }} className="text-center shadow border-5 m-2" border="primary">
-                        <Container class="container flex d-flex">
-                            <Spinner animation="border" variant="primary"className="m-3" />
-                            <Card.Title>Cargando...</Card.Title>
-                        </Container>
-                        <Card.Body>
-                            <Placeholder as={Card.Text} animation="glow">
-                                <Placeholder xs={7} /> <Placeholder xs={4} /> <Placeholder xs={4} />{' '}
-                                <Placeholder xs={6} /> <Placeholder xs={8} />
-                            </Placeholder>
-                            <Placeholder.Button variant="primary" xs={6} />
-                        </Card.Body>
-                    </Card> : products.map( prod => 
-            
-                    <Card key={prod.id} style={{ width: '18rem' }} className="m-2 text-center shadow border-5 fade-in" border="primary">
-                        <div className="overflow">
-                            <Card.Img variant="top" src={prod.pictureUrl} className="card-img-top"/>
-                        </div>
-                        <Card.Body className="bg-secondary">
-                            <Card.Title>{prod.title}</Card.Title>
-                            <Card.Text>
-                            Precio: $ {prod.price}
-                            </Card.Text>
-                            <Button variant="primary" className="shadow btn-primary w-100 mx-0">Ver detalles</Button>
-                        </Card.Body>
-                    </Card>
-            
-            )}
-        </Container>
+    <>
+        <Card key={producto.id} style={{ width: '18rem' }} className="m-2 text-center shadow border-5 fade-in" border="primary">
+                <div className="overflow">
+                    <Card.Img variant="top" src={producto.pictureUrl} className="card-img-top"/>
+                </div>
+                <Card.Body className="bg-secondary">
+                    <Card.Title>{producto.title}</Card.Title>
+                    <Card.Text>
+                    Precio: $ {producto.price}
+                    </Card.Text>
+                    <Button variant="primary" className="shadow btn-primary w-100 mx-0">Ver detalles</Button>
+                </Card.Body>
+        </Card>
+    </>
     )
 }
 
