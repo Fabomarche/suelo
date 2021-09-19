51dd87e7b99472b8144a67cd36ab1086b25e726f
Fabomarche <fabriciomarche@hotmail.com>
2021-09-19 16:02:11 -0300

    Primera entrega del Proyecto Final V4

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -60,44 +60,5 @@ const NavBar = () => {
     )
 }
 
-/* let stylesNav = {
-    display: 'flex',
-    backgroundColor: '#1D1D1B',
-    width: '100%',
-    justifyContent: 'center',
-    margin: '0',
-    padding: '0',
-
-}
-
-let stylesUl ={
-    display: 'flex',
-    listStyle: 'none',
-    alignItems: 'center'
-}
-
-let stylesLi={
-    padding: '0 2% 0 2%'
-}
-
-let styleImg={
-    width: '10%',
-    height: '100%'
-}
-
-const NavBar = () => {
-    return (
-        <div style={stylesNav}>
-            <img src={logoSuelo} style={styleImg} />
-            <h2>Suelo</h2>
-            <ul style={stylesUl}>
-                <li style={stylesLi}><a href="">Vegetales</a></li>
-                <li style={stylesLi}><a href="">Frutas</a></li>
-                <li style={stylesLi}><a href="">Sandwiches</a></li>
-                <li style={stylesLi}><a href="">Ensaladas</a></li>
-            </ul>
-        </div>
-    )
-} */
 
 export default NavBar
--- src/components/NavBar/UserWidget.jsx
+++ src/components/NavBar/UserWidget.jsx
@@ -4,10 +4,10 @@ import user from '../../assets/svg/user.svg'
 const UserWidget = () => {
     return (
         <>
-        <a href="#" className="d-block text-center mt-3 ms-md-5 me-1 me-md-5  me-md-0">
-         <img src={user} className="" alt="Log In" />
-         <p className="texto-iconos text-secondary">Log In</p>
-         </a>
+            <a href="#" className="d-block text-center mt-3 ms-md-5 me-1 me-md-5  me-md-0">
+                <img src={user} className="" alt="Log In" />
+                <p className="texto-iconos text-secondary">Log In</p>
+            </a>
         </>
     )
 }
